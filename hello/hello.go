4d08de39ffa732a13a57c961fcac8921c2bb087d
Ismaël Balaghni <20757786+ismaelbalaghni@users.noreply.github.com>
2022-09-28 22:16:05 +0200

    Add an input scanner for user input

--- hello/hello.go
+++ hello/hello.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"rsc.io/quote"
+	"bufio"
 	"example/greetings"
+	"fmt"
+	"os"
+	"rsc.io/quote"
 )
 
-func main(){
+func main() {
 	fmt.Println("Hello world")
 	fmt.Println(quote.Go())
 	// Utilisation du module greetings
 	message := greetings.Hello("Mark")
+	fmt.Println(message)
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Please enter your name: ")
+	scanner.Scan()
+	nom := scanner.Text()
+	if nom == "Joshua" {
+		message = greetings.Hello(nom + " Bledsoe")
+	} else if nom == "Drew" {
+		message = greetings.Hello(nom + " Bledsoe")
+	} else {
+		fmt.Println("The name you entered is unknown.")
+	}
+	//message = greetings.Hello(nom)
 	fmt.Println(message)
 }
