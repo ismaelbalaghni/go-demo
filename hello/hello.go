c5e766e59f711fb8f1908a7724f3f0cc1e7471c7
Ismaël Balaghni <20757786+ismaelbalaghni@users.noreply.github.com>
2022-09-28 16:56:21 +0200

    Add the greetings module

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"rsc.io/quote"
+	"example/greetings"
 )
 
 func main(){
 	fmt.Println("Hello world")
 	fmt.Println(quote.Go())
+	// Utilisation du module greetings
+	message := greetings.Hello("Mark")
+	fmt.Println(message)
 }
